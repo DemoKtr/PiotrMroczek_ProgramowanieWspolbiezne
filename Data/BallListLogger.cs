850bf68a8a87c95c60ea29912d000b501534f1ac
Piotr Mroczek <80900800+DemoKtr@users.noreply.github.com>
2023-05-28 14:52:15 +0200

    Add databallListLogger

---
+++ Data/BallListLogger.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+namespace Data
+{
+    internal class BallListLogger
+    {
+        public static string logFilePath = "%TEMP%/Balls/ballsLog.json";
+        private Task? loggingTask;
+        private ConcurrentQueue<IBall> ballQueue = new ConcurrentQueue<IBall>();
+        private readonly Mutex queueMutex = new Mutex();
+        private async Task LogToFile()
+        {
+            //If file doesnt exists create new one.
+            JArray array;
+            if (File.Exists(logFilePath))
+            {
+                array = JArray.Parse(File.ReadAllText(logFilePath));
+            }
+            else
+            {
+                array = new JArray();
+                File.Create(logFilePath);
+            }
+
+            //Append logs until queue empty
+            IBall ball;
+            while (ballQueue.TryDequeue(out ball))
+            {
+                JObject itemToAdd = JObject.FromObject(ball);
+                array.Add(itemToAdd);
+            }
+
+            // Convert data to string and save it
+            string output = JsonConvert.SerializeObject(array);
+            File.WriteAllText(logFilePath, output);
+        }
+        public async void AddToLogQueue(IBall ball)
+        {
+            queueMutex.WaitOne();
+            try
+            {
+                ballQueue.Enqueue(ball);
+            }
+            finally
+            {
+                queueMutex.ReleaseMutex();
+            }
+
+            if (loggingTask != null || loggingTask.IsCompleted.Equals("false"))
+                return;
+
+            loggingTask = Task.Run(this.LogToFile);
+        }
+    }
+}
+
